d99f8e6901620812556cead13c65b19b57b62b02
Lumora Tech Enterprise <lumoratech.ent@gmail.com>
2026-02-26 11:59:35 +0000

    Add printer settings and ESC/POS support

---
+++ src/types/esc-pos-encoder.d.ts
@@ -0,0 +1,13 @@
+declare module 'esc-pos-encoder' {
+  class EscPosEncoder {
+    initialize(): this;
+    align(align: 'left' | 'center' | 'right'): this;
+    size(width: number, height: number): this;
+    line(text: string): this;
+    text(text: string): this;
+    newline(): this;
+    cut(): this;
+    encode(): Uint8Array;
+  }
+  export default EscguPosEncoder;
+}
